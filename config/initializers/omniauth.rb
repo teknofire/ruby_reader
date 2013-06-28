b7c0e8dd3f18603475333c88335c491eb4839b8a
Will Fisher <will@teknofire.net>
2013-06-28 15:48:30 -0800

    update for fetchers and tmp files

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  require "openid/fetchers"
   require 'openid/store/filesystem' 
-  provider :open_id, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id', :store => OpenID::Store::Filesystem.new("#{Rails.root}/tmp")
+
+  OpenID.fetcher.ca_file = "/etc/ssl/certs/ca-bundle.trust.crt"  
+  provider :open_id, :name => 'google', :identifier => 'https://www.google.com/accounts/o8/id', :store => OpenID::Store::Filesystem.new("./tmp")
 end
